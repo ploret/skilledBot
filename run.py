bebbf6b95b46d58cf6516b0831c80c95d2facad7
lemo_ua <plorett@yandex.ru>
2016-05-07 11:56:48 +0300

    created project file structure

--- run.py
+++ run.py
@@ -1,2 +1,16 @@
+#!/usr/bin/python
 
-print("test")
+import telebot
+import sys
+
+if len(sys.argv) == 1:
+    sys.exit("Usage %s token" % sys.argv[0])
+
+token = sys.argv[1]
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(func=lambda message: message.text == "help")
+def my_func(m):
+    bot.send_message(m.chat.id, "My respond")
+
+bot.polling()
